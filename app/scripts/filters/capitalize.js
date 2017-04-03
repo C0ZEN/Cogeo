2e87d9da09f18fcd41a80f13fcb3e90f8f384c6a
Geoffrey Testelin <geoffrey.testelin@gmail.com>
2017-04-03 11:08:49 +0200

    Refacto of capitalize

--- app/scripts/filters/capitalize.js
+++ app/scripts/filters/capitalize.js
@@ -1,3 +1,9 @@
+/**
+ * @description
+ * Transform the text as lowercase and then add uppercase
+ * Note: You should use yourText.trim() before calling the filter to avoid unexpected behavior
+ *
+ */
 (function (angular) {
     'use strict';
 
@@ -5,14 +11,35 @@
         .module('4pjtApp')
         .filter('capitalize', capitalize);
 
-    function capitalize() {
+    capitalize.$inject = [
+        'PublicMethods'
+    ];
+
+    function capitalize(PublicMethods) {
         return capitalizeFilter;
 
-        function capitalizeFilter(input, all) {
+        /**
+         * @param {string}  text                  > The text you want to convert
+         * @param {boolean} all           = false > Check for the whole text
+         * @param {boolean} firstCharOnly = false > Capitalize only the first letter
+         */
+        function capitalizeFilter(text, all, firstCharOnly) {
             var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
-            return (!!input) ? input.replace(reg, function (txt) {
-                    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-                }) : '';
+            if (!PublicMethods.isNullOrEmpty(text)) {
+                if (firstCharOnly) {
+                    text = text.toLowerCase();
+                    text = text.charAt(0).toUpperCase() + text.slice(1);
+                }
+                else {
+                    text.replace(reg, function (txt) {
+                        text = txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                    });
+                }
+                return text;
+            }
+            else {
+                return '';
+            }
         }
     }
 
