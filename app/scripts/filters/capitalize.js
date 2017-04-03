cbd8b882ea9e5ba257a1271176f51f79e326a441
Geoffrey Testelin <geoffrey.testelin@gmail.com>
2017-04-03 11:07:31 +0200

    Refacto of capitalize

--- app/scripts/filters/capitalize.js
+++ app/scripts/filters/capitalize.js
@@ -10,7 +10,9 @@
 
         function capitalizeFilter(input, all) {
             var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
-            return (!!input) ? input.replace(reg, function (txt) {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
+            return (!!input) ? input.replace(reg, function (txt) {
+                    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                }) : '';
         }
     }
 
